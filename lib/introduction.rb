6ecb14567733464e48b0fc871a73f72e453c231a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-22 21:35:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction ("Josh")
-  puts "Hi, my name is #{name}\n."
+  puts "Hi, my name is #{name}.\n"
 end
 
 def introduction ("Dan", "Ember.js")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}\n."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
