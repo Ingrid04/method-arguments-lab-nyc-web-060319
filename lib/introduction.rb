5bac6a41af64e7cc539a0c5adf170103a246d619
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-22 21:31:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
-def introduction (Josh)
+def introduction ("Josh")
   puts "Hi, my name is #{name}."
 end
 
-def introduction (Ingrid, Ruby)
+def introduction ("Ingrid", "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
